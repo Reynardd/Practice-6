badbeff677bfcc3b7e91af07b0945d4c76e9d864
MohammadHossein R Shojaodiny <83103488+Reynardd@users.noreply.github.com>
2022-12-03 14:05:41 +0330

    Update 02.beSalaamatie.c

--- 02.beSalaamatie.c
+++ 02.beSalaamatie.c
@@ -1,37 +1,22 @@
 #include <stdio.h>
-int greenCount(char word[6]){
+int colorCount(char word[6],char color){
     int c=0;
     for (int i = 0; i < 6; i++)
     {
-        if (word[i]=='G')
+        if (word[i]==color)
         {
             c++;
         }
     }
     return c;
 }
-int redCount(char word[6]){
-    int c=0;
-    for (int i = 0; i < 6; i++)
-    {
-        if (word[i]=='R')
-        {
-            c++;
-        }
-    }
-    return c;
-}
-void n(){
-    printf("nakhor lite");
-}
-void b(){
-    printf("rahat baash");
-}
+void n(){printf("nakhor lite");}
+void b(){printf("rahat baash");}
 int main(){
     char tag[6];
     scanf("%s",tag);
-    int g=greenCount(tag);
-    int r=redCount(tag);
+    int g=colorCount(tag,'G');
+    int r=colorCount(tag,'R');
     int y=5-g-r;
     if (r>=3)n();
     else if (r>=2 && y>=2)n();
